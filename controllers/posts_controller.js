e74c7bca855e4a7fcdf1f833a03e5dd47ec9a541
Shubham <ss8853895989@gmail.com>
2020-05-04 16:13:24 +0530

    posts controller added

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.timeline = function(req,res){
+    return res.end('<h1>your posts will be visible here</h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ console.log('router loaded');
 
 router.get('/',homeController.home);
 
+router.use('/users', require('./users'));
+
+router.use('/posts', require('./posts'));
+
+//for any other further routes, access from here
+// router.use('/routerName', require('./routerFile'))
+
+
 module.exports = router;
