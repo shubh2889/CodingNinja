7c4d9d8391c3ad49ae5db3bc2689fcf440f1d843
Shubham <ss8853895989@gmail.com>
2020-05-10 18:30:54 +0530

    layout added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 // use express router
 
-app.use('/',require('./routes/index'))
+app.use('/',require('./routes/index'));
+
+// setup the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
